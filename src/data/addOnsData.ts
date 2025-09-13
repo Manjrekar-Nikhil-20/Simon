a6681cabf84f6eeeeae298237e4168a7235fea1e
Manjrekar-Nikhil-20 <manjrekarnikhil18@gmail.com>
2025-09-13 21:30:53 +0530

    Update Add-ons Data with Specified Changes

---
+++ src/data/addOnsData.ts
@@ -0,0 +1,155 @@
+@@ .. @@
+ export const addOns: AddOn[] = [
+-  {
+-    id: '1',
+-    name: 'Rose',
+-    price: 99,
+-    image:
+-      'https://i.pinimg.com/736x/ed/fe/5a/edfe5a4351b8ba8db94863713f25945a.jpg',
+-  },
+   {
+-    id: '2',
++    id: '1',
+     name: 'LED HBD',
+     price: 119,
+     image:
+       'https://i.pinimg.com/736x/24/5b/cc/245bcc1f3f850d23301416b4cfe8268f.jpg',
+   },
+   {
+-    id: '3',
++    id: '2',
+     name: 'Fog Entry',
+-    price: 899,
++    price: 700,
+     image:
+       'https://i.pinimg.com/736x/2c/b4/d4/2cb4d454ec94675ab6aaf3a23d537cce.jpg',
+   },
+   {
+-    id: '4',
++    id: '3',
+     name: 'Fog Entry + Cold Fire (2)',
+-    price: 1599,
++    price: 1400,
+     image:
+       'https://i.pinimg.com/736x/2c/b4/d4/2cb4d454ec94675ab6aaf3a23d537cce.jpg',
+   },
+   {
+-    id: '5',
+-    name: 'Fog Entry + Cold Fire (4)',
+-    price: 2299,
+-    image:
+-      'https://i.pinimg.com/736x/2c/b4/d4/2cb4d454ec94675ab6aaf3a23d537cce.jpg',
+-  },
+-  {
+-    id: '6',
++    id: '4',
+     name: 'Photo Props',
+     price: 189,
+     image:
+       'https://i.pinimg.com/736x/20/63/0e/20630ecad25db83d32774bb1b2c175c3.jpg',
+   },
+   {
+-    id: '7',
+-    name: 'Bouquet',
+-    price: 499,
+-    image:
+-      'https://i.pinimg.com/736x/92/7e/5c/927e5c6d01965b9ea9e898ad410c39e1.jpg',
+-  },
+-  {
+-    id: '8',
++    id: '5',
+     name: 'LED Name Letters',
+     price: 299,
+     image:
+       'https://i.pinimg.com/736x/5e/55/6c/5e556c368ccca863b363a2ce5eb45047.jpg',
+   },
+   {
+-    id: '9',
++    id: '6',
+     name: 'Table Décor',
+-    price: 349,
++    price: 299,
+     image:
+       'https://i.pinimg.com/736x/c9/29/38/c92938bd766aad79e76bdf0372eb5c7c.jpg',
+   },
+   {
+-    id: '10',
++    id: '7',
+     name: 'Candles',
+     price: 199,
+     image:
+       'https://i.pinimg.com/736x/cf/45/65/cf4565965b293db6bfaa32e466fe81e7.jpg',
+   },
+   {
+-    id: '11',
++    id: '8',
+     name: 'Photoshoot (30 mins)',
+     price: 600,
+     image:
+       'https://i.pinimg.com/736x/90/fc/9a/90fc9ab1deee5e949f6821ce9b0ce1c3.jpg',
+   },
+   {
+-    id: '12',
++    id: '9',
+     name: 'Photoshoot (60 mins)',
+     price: 1200,
+     image:
+       'https://i.pinimg.com/736x/90/fc/9a/90fc9ab1deee5e949f6821ce9b0ce1c3.jpg',
+   },
+   {
+-    id: '13',
++    id: '10',
+     name: 'Sash & Crown',
+     price: 199,
+     image:
+       'https://i.pinimg.com/736x/39/6b/d0/396bd0c5605d516457c8bd788659c60f.jpg',
+   },
+   {
+-    id: '14',
++    id: '11',
+     name: 'Cold Fire',
+     price: 700,
+     image:
+       'https://i.pinimg.com/736x/bc/19/84/bc1984a1557b2f4809e8975810d5556e.jpg',
+   },
+   {
+-    id: '15',
++    id: '12',
+     name: 'Candle Faith',
+     price: 199,
+     image:
+       'https://i.pinimg.com/736x/fa/b6/b6/fab6b6d775637cfdfdad3ff2ee401b17.jpg',
+   },
+   {
+-    id: '16',
+-    name: 'Fog Effect',
++    id: '13',
++    name: 'Fog in Room',
+     price: 499,
+     image:
+       'https://i.pinimg.com/736x/e2/23/72/e22372b9774e96573ea2da424a8f7c1e.jpg',
+   },
+   {
+-    id: '17',
++    id: '14',
+     name: 'LOVE',
+     price: 99,
+     image:
+       'https://i.pinimg.com/736x/f0/4e/04/f04e042d1645e3642e139d6614d3fc34.jpg',
+   },
+   {
+-    id: '18',
++    id: '15',
+     name: 'LED Numbers',
+     price: 99,
+     image:
+       'https://i.pinimg.com/736x/1b/63/74/1b63740cf559f6235ff780140a8f6e93.jpg',
+   },
++  {
++    id: '16',
++    name: 'Bubble Entry',
++    price: 200,
++    image:
++      'https://images.pexels.com/photos/1319854/pexels-photo-1319854.jpeg',
++  },
+ ];
